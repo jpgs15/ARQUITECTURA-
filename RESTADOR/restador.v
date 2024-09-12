e522ee4cd437342532c18810a3ccfcaf15388023
jpgs15 <pablo.jpe@hotmail.com>
2024-09-11 22:55:41 -0500

    update

--- RESTADOR/restador.v
+++ RESTADOR/restador.v
@@ -1,17 +1,27 @@
+`include "sum4b.v"
 module restador(
     input [3:0] a,       // entrada 1 de 4 bits
     input [3:0] b,       // entrada 2 de 4 bits
-    output [3:0] diff,   // resta salida
-    output carri_out    //  acarreo salida de cada bit.
+    input sel,           // selector
+    output Cout,    //  acarreo salida de cada bit.
+    output [3:0] Rest  // resta salida
 );
-
     // Señal interna para el acarreo de salida de cada bit
-    wire [3:0] carri;
+     wire C1;
+     wire b0,b1,b2,b3;
+     wire C_out;
     
-    // Restador de 1 bit  
-    assign {carri[0], diff[0]} = a[0] - b[0];
-    assign {carri[1], diff[1]} = a[1] - b[1] - carri[0];
-    assign {carri[2], diff[2]} = a[2] - b[2] - carri[1];
-    assign {carri_out, diff[3]} = a[3] - b[3] - carri[2];
+     assign b0 = b[0]^sel;
+     assign b1 = b[1]^sel;
+     assign b2 = b[2]^sel;
+     assign b3 = b[3]^sel;
+
+     sum4b s0 (.a(a), .b({b3,b2,b1,b0}), .ci(sel), .Cout(Cout) ,)
+
+     case (status)
+     NEG:
+     default:
+     
+     endcase
 
 endmodule
